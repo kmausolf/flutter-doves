9f6bad672691c88aae2464ea2823a38b56fbd781
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2020-03-03 15:34:59 -0800

    fix(environment): Fixes project name

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kendrickmausolf/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kendrickmausolf/Documents/flutter_tech_demo"
-export "FLUTTER_TARGET=/Users/kendrickmausolf/Documents/flutter_tech_demo/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/kendrickmausolf/Documents/GitHub/flutter-doves"
+export "FLUTTER_TARGET=/Users/kendrickmausolf/Documents/GitHub/flutter-doves/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kendrickmausolf/Documents/flutter/bin/cache/artifacts/engine/ios"
